bd6701557c870f008fc3d29a9a71d35ecc3e99c7
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2021-12-19 11:10:45 +0100

    adding prject state management

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 console.log("In app.ts");
+// Poject State Management
+class ProjectState {
+    constructor() {
+        this.projects = [];
+    }
+    addProject(title, description, numOfPeople) {
+        const newProject = {
+            id: Math.random().toString(),
+            title: title,
+            description: description,
+            people: numOfPeople
+        };
+        this.projects.push(newProject);
+    }
+}
 function validate(validatableInput) {
     let isValid = true;
     if (validatableInput.required) {
@@ -44,6 +59,27 @@ function autobind(_, _2, descriptor) {
     return adjDescriptor;
 }
 // ProjectList Class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent =
+            this.type.toUpperCase() + ' Projects';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 //ProjectInput Class
 class ProjectInput {
     constructor() {
@@ -112,3 +148,5 @@ __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput;
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
 console.log("In app.ts")
+// Poject State Management
+class ProjectState {
+    private projects: any[] = []
+
+    addProject(title: string, description: string, numOfPeople: number) {
+        const newProject = {
+            id: Math.random().toString(),
+            title: title,
+            description: description,
+            people: numOfPeople
+        }
+
+        this.projects.push(newProject)
+    }
+
+}
+
 //Validation
 interface Validatable {
     value: string | number
@@ -61,7 +78,42 @@ function autobind(
 }
 
 // ProjectList Class
+class ProjectList {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    element: HTMLElement
 
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById(
+            'project-list'
+        )! as HTMLTemplateElement
+        this.hostElement = document.getElementById('app')! as HTMLDivElement
+
+        const importedNode = document.importNode(
+            this.templateElement.content, 
+            true
+        )
+        this.element = importedNode.firstElementChild as HTMLElement
+        this.element.id = `${this.type}-projects`
+        
+        this.attach()
+        this.renderContent()
+
+    }
+
+
+
+    private renderContent() {
+        const listId = `${this.type}-project-list`
+        this.element.querySelector('ul')!.id = listId
+        this.element.querySelector('h2')!.textContent = 
+        this.type.toUpperCase() + ' Projects'
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element)
+    }
+}
 
 //ProjectInput Class
 class ProjectInput {
@@ -150,3 +202,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput
+const activePrjList = new ProjectList('active')
+const finishedPrjList = new ProjectList('finished')
